620035c89f6a19b6bffe138e4f473394b1e7aa03
VasilevichP <143924623+VasilevichP@users.noreply.github.com>
2023-09-28 17:07:56 +0300

    Update Task 3.py

--- Task 3.py
+++ Task 3.py
@@ -4,30 +4,16 @@ import numpy as np
 
 
 def find_max_index(arr):
-    max_value = arr[0][0]
-    max_row = 0
-    max_col = 0
-
-    for row in range(len(arr)):
-         k=max(arr[row])
-         if k>max_value:
-            max_value=k
-            max_row=row
-            max_col=arr[row].index(k)
-
-    print("Индексы первого вхождения максимального элемента: [", max_row,"][", max_col,"]")
+    max_row=max(arr,key=max)
+    return arr.index(max_row),max_row.index(max(max_row))
 
 
 rows = random.randint(1, 6)
 print("Количество строк:", rows)
 cols = random.randint(1, 6)
 print("Количество столбцов:", cols)
-matrix = []
+matrix=[]
 for i in range(rows):
-    row = []
-    for j in range(cols):
-        a = random.randint(10, 99)
-        row.append(a)
-    matrix.append(row)
-    print(row)
-find_max_index(matrix)
+    matrix.append([random.randint(1,99) for i in range(cols)])
+print(matrix)
+print(find_max_index(matrix))
